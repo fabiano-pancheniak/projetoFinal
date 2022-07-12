0ae4ce6ad09ddee21fa795aca1d14d8d648656a6
Fabiano Pancheniak <fabiano.filho@aluno.unc.br>
2022-07-12 15:13:43 -0300

    contador e data funcionando, hora falta deixar dinamica

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,6 +3,14 @@ import styles from './Footer.module.scss';
 
 
 export default function Footer(){
+
+    const [counter, setCounter] = React.useState(60);
+    React.useEffect(() => {
+        counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
+        if(counter == 0 ){
+            setCounter(counter + 60)
+        }
+      }, [counter]);
     return(
         <div className={styles.footer}> 
             <p className={styles.text}>Essa janela do navegador é usada para manter sua sessão de autenticação ativa. Deixe-a aberta em segundo plano e abra uma nova janela para continuar a navegar.</p>
@@ -10,7 +18,7 @@ export default function Footer(){
             <div className={styles.countdownTextRefresh}> Application refresh in</div>
             <div className={styles.countdown}>
                 <div className={styles.countdownNumberText}>
-                <div className={styles.countdownNumber}> 60 </div>
+                <div className={styles.countdownNumber}> {counter} </div>
                 <div className={styles.countdownTextSeconds}> seconds </div>
                 </div>
             </div>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,15 +4,51 @@ import logo from "../../assets/logo2.png";
 import cloudy from "../../assets/cloudy.svg";
 
 
+
 export default function Header(){
+
+    const weekday = ["Domingo","Segunda-feira","Terça-feira","Quarta-feira","Quinta-feira","Sexta-feira","Sábado"];
+    const monthTxt = 
+    ["janeiro",
+    "fevereiro",
+    "março",
+    "abril",
+    "maio",
+    "junho",
+    "julho",
+    "agosto",
+    "setembro",
+    "outubro",
+    "novembro",
+    "dezembro"];
+
+    const d = new Date();
+  
+    const hour = d.getHours();
+    const min = d.getMinutes();
+    let day = weekday[d.getDay()];
+    let month = monthTxt[d.getMonth()];
+    let year = d.getFullYear();
+    let dayNumber = d.getDay();
+
+    
+
+    
+  
+  
+    
+    
+    
+   
+
     return(
         <header className={styles.header}>
         <div className={styles.headerImg}>
             <img src={logo} alt="Logo"></img>
         </div>
         <div className={styles.time}>
-            <div className={styles.hour}> 08:07</div>
-            <div className={styles.date}> terça feira, 12 de julho de 2022</div>
+            <div className={styles.hour}>{hour}:{min}</div>
+            <div className={styles.date}> {day}, {dayNumber} de {month} de {year}</div>
         </div>
         <div className={styles.locationTemp}>
             <div className={styles.location}> Passo Fundo - RS</div>
